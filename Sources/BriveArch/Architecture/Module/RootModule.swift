09f9b1b180a083c0613566cd5376db844e2d920b
gosha <gosha.titov@icloud.com>
2022-11-16 04:17:17 +0300

    Feat: add root builder and root logger.

--- Sources/BriveArch/Architecture/Module/RootModule.swift
+++ Sources/BriveArch/Architecture/Module/RootModule.swift
@@ -1,7 +1,33 @@
+import UIKit
+
 open class RootModule {
     
-    let name: String = "root"
+    public enum LogLevel {
+        case info, warning
+    }
     
-    public init() {}
+    public let name: String = "root"
+    
+    /// An object that logs messages of all modules that grow from this root module.
+    ///
+    /// You can set your own logger, just implement a new class that subclasses  the `ModuleLogger` class.
+    public var logger: ModuleLoggerProtocol = ConsoleModuleLogger()
+    
+    /// An initial child module from which the module tree begins.
+    private let child: Module
+    
+    
+    public final func launch(from window: UIWindow) -> Void {
+        
+    }
+    
+    
+    // MARK: - Init
+    
+    public init(builder: RootBuilderProtocol) {
+        child = builder.createInitialModule()
+        child.logger = logger
+        child.load()
+    }
     
 }
